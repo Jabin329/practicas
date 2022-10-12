19f39ea5a932a1b934aeafe74995b9695a775df9
Jabin329 <115598879+Jabin329@users.noreply.github.com>
2022-10-11 20:16:38 -0500

    Add files via upload

---
+++ datosp.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<?php
+
+
+
+
+
+	echo "Nombre(s): ". $OtroArray[nombre(s)];
+	echo "Apellido paterno: ". $OtroArray[apellidop];
+	echo "Apellido materno: ". $OtroArray[apellidom];
+	echo "Direccion: ". $OtroArray[direccion];
+	echo "Direccion: ". $OtroArray[telefono];
+?>
+
+</body>
+</html>
+	
